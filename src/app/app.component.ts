f1d3aa71dadd31640553224a96a9b16088f26213
Tim <tim@xcel.digital>
2020-05-15 23:33:17 -0700

    Aded getCurrentUser()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FirebaseCoreService } from './angularx/shared/services/firebase/firebase-core.service';
-import { AngularXUserService } from './angularx/shared/services/angularx-user/angularx-user.service';
-import { Router, NavigationEnd } from '@angular/router';
-import { AngularXDialogService } from './angularx/shared/services/angularx-dialog/angularx-dialog.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
     title = 'angularx';
-    constructor(private _userService: AngularXUserService,
-                private _firebaseCore: FirebaseCoreService,
-                private _router: Router,
-                private _dialogService: AngularXDialogService) {}
-
-    ngOnInit() {
-
-        //Activated whenever a page is loaded
-        this._router.events.subscribe($event => {
-            if ($event instanceof NavigationEnd) {
-                this.initCurrentUser();
-            }
-        });
-
-    }
-
-    initCurrentUser() {
-        this._userService.getCurrentUser().subscribe(currentUser => {
-            //Load the current user only if it hasn't been loaded
-            if (currentUser !== null) return;
-
-            
-            let firebase = this._firebaseCore.getFirebaseInstance();
-            let firebaseCurrentUser = firebase.auth().currentUser;
-
-            //Cannot load the current user if the user isn't logged in
-            if (!firebaseCurrentUser) return;
-
-            let uid = firebaseCurrentUser.uid;
-            this._userService.getUser(uid, true, true).subscribe(
-                () => {},
-                error => {
-                    this._dialogService.alert({
-                        title: 'An Error Has Occurred',
-                        message: error
-                    });
-                }
-            );
-        });
+    constructor() {
+        
     }
     
 }
